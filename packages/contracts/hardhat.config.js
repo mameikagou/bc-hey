a3fe5b57c6f3d5ab40a5c30a8f95be9a70831929
mrlonely <mrlonely1226@163.com>
2025-01-03 10:35:36 +0800

    feat:添加部署到测试网的程序

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -7,5 +8,11 @@ module.exports = {
     sources: "./contracts",
     tests: "./test",
     artifacts: "./artifacts"
+  },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
   }
 };
