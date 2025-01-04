96bde7f86a278f8a2006f7d1349ac9db3ba30eec
mrlonely <mrlonely1226@163.com>
2025-01-04 11:33:39 +0800

    feat:测试代码

--- packages/contracts/test/RewardDistribution.test.js
+++ packages/contracts/test/RewardDistribution.test.js
@@ -1,45 +1,103 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("RewardDistribution Contract", function () {
-  let RewardDistribution, rewardDistribution, MockERC20, rewardToken;
-  let admin, user1, user2;
+describe("RewardDistribution", function () {
+    let RewardDistribution, rewardDistribution;
+    let MockERC20, rewardToken;
+    let admin, user1, user2;
+    const initialSupply = ethers.parseEther("10000");
+    const rewardAmount = ethers.parseEther("100");
 
-  beforeEach(async function () {
-    // 获取 signers
-    [admin, user1, user2] = await ethers.getSigners();
+    beforeEach(async function () {
+        // 获取测试账户
+        [admin, user1, user2] = await ethers.getSigners();
 
-    // 部署 MockERC20 合约
-    MockERC20 = await ethers.getContractFactory("MockERC20");
-    rewardToken = await MockERC20.deploy(
-      "RewardToken", // name
-      "RWT",         // symbol
-      18,            // decimals
-      ethers.utils.parseEther("10000") // initial supply
-    );
-    await rewardToken.deployed();
+        // 部署 MockERC20 代币
+        MockERC20 = await ethers.getContractFactory("MockERC20");
+        rewardToken = await MockERC20.deploy(
+            "Reward Token",
+            "RWD",
+            initialSupply
+        );
+        await rewardToken.waitForDeployment();
 
-    // 部署 RewardDistribution 合约
-    RewardDistribution = await ethers.getContractFactory("RewardDistribution");
-    rewardDistribution = await RewardDistribution.deploy(rewardToken.address);
-    await rewardDistribution.deployed();
+        // 部署奖励分发合约
+        RewardDistribution = await ethers.getContractFactory("RewardDistribution");
+        rewardDistribution = await RewardDistribution.deploy(await rewardToken.getAddress());
+        await rewardDistribution.waitForDeployment();
 
-    // 将奖励代币转移到 RewardDistribution 合约
-    await rewardToken.transfer(rewardDistribution.address, ethers.utils.parseEther("5000"));
-  });
+        // 转移代币到奖励合约
+        await rewardToken.transfer(
+            await rewardDistribution.getAddress(), 
+            ethers.parseEther("5000")
+        );
+    });
 
-  it("should allocate rewards correctly", async function () {
-    await rewardDistribution.allocateReward(user1.address, ethers.utils.parseEther("100"));
-    const reward = await rewardDistribution.getUserReward(user1.address);
-    expect(reward).to.equal(ethers.utils.parseEther("100"));
-  });
+    describe("Basic Functions", function () {
+        it("Should set the right admin", async function () {
+            expect(await rewardDistribution.admin()).to.equal(admin.address);
+        });
 
-  it("should allow users to claim rewards", async function () {
-    await rewardDistribution.allocateReward(user1.address, ethers.utils.parseEther("100"));
-    await rewardDistribution.connect(user1).claimReward();
-    const reward = await rewardDistribution.getUserReward(user1.address);
-    expect(reward).to.equal(ethers.utils.parseEther("0")); 
-    const userBalance = await rewardToken.balanceOf(user1.address);
-    expect(userBalance).to.equal(ethers.utils.parseEther("100"));
-  });
+        it("Should set the right token", async function () {
+            expect(await rewardDistribution.rewardToken()).to.equal(
+                await rewardToken.getAddress()
+            );
+        });
+    });
+
+    describe("Reward Allocation", function () {
+        it("Should allocate rewards correctly", async function () {
+            await rewardDistribution.allocateReward(
+                user1.address, 
+                rewardAmount
+            );
+            
+            const reward = await rewardDistribution.getUserReward(user1.address);
+            expect(reward).to.equal(rewardAmount);
+        });
+
+        it("Should emit RewardAllocated event", async function () {
+            const tx = await rewardDistribution.allocateReward(user1.address, rewardAmount);
+            const receipt = await tx.wait();
+            
+            const event = receipt.logs[0];
+            const decodedEvent = rewardDistribution.interface.parseLog(event);
+            
+            expect(decodedEvent.name).to.equal("RewardAllocated");
+            expect(decodedEvent.args[0]).to.equal(user1.address);
+            expect(decodedEvent.args[1]).to.equal(rewardAmount);
+        });
+
+        it("Should fail if non-admin tries to allocate", async function () {
+            await expect(
+                rewardDistribution.connect(user1).allocateReward(user2.address, rewardAmount)
+            ).to.be.revertedWith("Only admin can call this function");
+        });
+    });
+
+    describe("Reward Claiming", function () {
+        beforeEach(async function () {
+            await rewardDistribution.allocateReward(user1.address, rewardAmount);
+        });
+
+        it("Should emit RewardClaimed event", async function () {
+            const tx = await rewardDistribution.connect(user1).claimReward();
+            const receipt = await tx.wait();
+            
+            // 找到 RewardClaimed 事件
+            const rewardClaimedEvent = receipt.logs.find(log => {
+                try {
+                    const decoded = rewardDistribution.interface.parseLog(log);
+                    return decoded.name === "RewardClaimed";
+                } catch {
+                    return false;
+                }
+            });
+            
+            const decodedEvent = rewardDistribution.interface.parseLog(rewardClaimedEvent);
+            expect(decodedEvent.name).to.equal("RewardClaimed");
+            expect(decodedEvent.args[0]).to.equal(user1.address);
+            expect(decodedEvent.args[1]).to.equal(rewardAmount);
+        });
+    });
 });
